31d7b1500f75bd00547cc5ec7b1ee240b0bae5a2
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-08 02:21:57 -0800

    Update botnumber.lua

--- plugins/botnumber.lua
+++ plugins/botnumber.lua
@@ -1,12 +1,12 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+19164368923", "DARKviRus", "BOT", ok_cb, false)
+send_contact(get_receiver(msg), "6you LO6SE BABY6", "Error oops", "BOT", ok_cb, false)
 end
 
 return {
 patterns = {
-"^!botnumber$"
+"^botnumber$"
 
 },
 run = run
